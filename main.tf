33b2726e68c3460ebcd39965e1585c7f28290522
EC2 Default User <ec2-user@ip-172-31-47-163.eu-central-1.compute.internal>
2019-08-14 10:52:22 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+	region = "eu-central-1"
+}
+
+resource "aws_instance" "poc-server" {
+	ami="ami-0cc293023f983ed53"
+	instance_type="t2.micro"
+
+	tags = {
+	Name = "poc-server1"
+	}
+}
