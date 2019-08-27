c389a836a99498b56f7df56f2a69922a2a66c2e8
greniu555 <54064042+greniu555@users.noreply.github.com>
2019-08-27 12:21:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
 	region = "eu-central-1"
 	#access_key = "ASIAW3BYK4VKAYKPDVFH"
 	#secret_key = "59nGCe0+X8rBkIHe6CIHz1ThUNBSbC5EAYM5WxSR"
+	shared_credentials_file = "/home/ec2-user/.aws/credentials"
 }
 
 
