173bbf38cd0031f5680b183539dfbd7735c20d10
greniu555 <54064042+greniu555@users.noreply.github.com>
2019-08-27 11:56:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 	region = "eu-central-1"
-	access_key = "ASIAW3BYK4VKAYKPDVFH"
-	secret_key = "59nGCe0+X8rBkIHe6CIHz1ThUNBSbC5EAYM5WxSR"
+	#access_key = "ASIAW3BYK4VKAYKPDVFH"
+	#secret_key = "59nGCe0+X8rBkIHe6CIHz1ThUNBSbC5EAYM5WxSR"
 }
 
 
